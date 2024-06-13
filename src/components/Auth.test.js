fc7e6958b4dc3fe0489004e12b72e53cdeb12235
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-13 16:33:02 +0530

    Create Auth.test.js

---
+++ src/components/Auth.test.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { AuthProvider } from './AuthContext';
+import Auth from './Auth';
+
+test('should render authentication status and checkbox', () => {
+  render(
+    <AuthProvider>
+      <Auth />
+    </AuthProvider>
+  );
+
+  // Check if the p element with class authText is in the document
+  const authText = screen.getByText(/You are not authenticated/i);
+  expect(authText).toBeInTheDocument();
+
+  const checkbox = screen.getByLabelText(/I am not a robot/i);
+  expect(checkbox).toBeInTheDocument();
+
+  fireEvent.click(checkbox);
+  expect(screen.getByText(/You are authenticated/i)).toBeInTheDocument();
+
+  fireEvent.click(checkbox);
+  expect(screen.getByText(/You are not authenticated/i)).toBeInTheDocument();
+});
